61c10382d57f5f562b9f2dcda582891046938e70
FacticiusVir <FacticiusVir@gmail.com>
2017-02-07 19:58:46 +0000

    Add project files.

---
+++ Keeper.DotMudCore.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Keeper.DotMudCore.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

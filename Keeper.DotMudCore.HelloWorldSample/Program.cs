8acc42061953ef9b99c3ac9fcb575e88825a32c0
Andrew Armstrong <FacticiusVir@gmail.com>
2017-02-15 12:00:06 +0000

    Added Hello World sample

---
+++ Keeper.DotMudCore.HelloWorldSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Keeper.DotMudCore.HelloWorldSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new MudHostBuilder()
+                            .ConfigureServices(services => services.AddTcpEndpoint(options => options.Port = 5000))
+                            .Configure(server => server.Run(async session =>
+                            {
+                                await session.Connection.SendLineAsync("Hello World!");
+                                await session.Connection.ReceiveLineAsync();
+                            }))
+                            .Build();
+
+            Console.WriteLine("Running");
+
+            host.Run();
+        }
+    }
+}
